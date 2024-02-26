7316a070012ef39e14914f582c6b5ce4083d369e
blast <fberrzig@gmail.com>
2024-02-26 19:55:34 +0100

    added database connection

--- AM.ApplicationCore/Domain/Passenger.cs
+++ AM.ApplicationCore/Domain/Passenger.cs
@@ -2,6 +2,7 @@
 {
     public class Passenger
     {
+        public int PassengerId { get; set; }
         public string PassportNumber { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
