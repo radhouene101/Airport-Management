99582b4ecab57698904f1332f5cd9b64620ed7ef
radhouene101 <radhouene.berrzig@esprit.tn>
2024-04-01 22:19:04 +0100

    data porter

---
+++ AM.Infrastructure/Migrations/20240401170506_porteuse-de-donnee.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class porteusededonnee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservations");
+
+            migrationBuilder.CreateTable(
+                name: "Ticket",
+                columns: table => new
+                {
+                    PassengerFK = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FlightFk = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Prix = table.Column<double>(type: "float", nullable: false),
+                    Siege = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VIP = table.Column<bool>(type: "bit", nullable: false),
+                    FightFlightId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ticket", x => new { x.FlightFk, x.PassengerFK });
+                    table.ForeignKey(
+                        name: "FK_Ticket_Flights_FightFlightId",
+                        column: x => x.FightFlightId,
+                        principalTable: "Flights",
+                        principalColumn: "FlightId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ticket_Passengers_PassengerFK",
+                        column: x => x.PassengerFK,
+                        principalTable: "Passengers",
+                        principalColumn: "PassportNumber",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_FightFlightId",
+                table: "Ticket",
+                column: "FightFlightId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_PassengerFK",
+                table: "Ticket",
+                column: "PassengerFK");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ticket");
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    FlightsFlightId = table.Column<int>(type: "int", nullable: false),
+                    PassengersPassportNumber = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => new { x.FlightsFlightId, x.PassengersPassportNumber });
+                    table.ForeignKey(
+                        name: "FK_Reservations_Flights_FlightsFlightId",
+                        column: x => x.FlightsFlightId,
+                        principalTable: "Flights",
+                        principalColumn: "FlightId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Passengers_PassengersPassportNumber",
+                        column: x => x.PassengersPassportNumber,
+                        principalTable: "Passengers",
+                        principalColumn: "PassportNumber",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_PassengersPassportNumber",
+                table: "Reservations",
+                column: "PassengersPassportNumber");
+        }
+    }
+}
